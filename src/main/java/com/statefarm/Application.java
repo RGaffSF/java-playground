a79594d31c83bc05b30d2b876a1687e6f1119495
Randall <randall.gaffner.t1w9@statefarm.com>
2021-08-16 15:25:20 -0500

    finished command line example

--- src/main/java/com/statefarm/Application.java
+++ src/main/java/com/statefarm/Application.java
@@ -2,6 +2,14 @@ package com.statefarm;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("hello world");
-    }
+        if (args.length < 1) {
+            System.out.println("Please specify a name and email");
+        } else if (args.length == 1) {
+            System.out.println("Please specify an email for Robert");
+        } else {
+            System.out.println(String.format(args[0]) + " <" + args[1] + ">");
+        }
+
+
+}
 }
